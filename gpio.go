aa9984f06b4c9f63ace91b2ba015e964e1b530e2
Breandan Dezendorf <breandan@dezendorf.com>
2020-03-11 10:05:38 -0400

    Whitespace cleanup and general flow fixes

--- gpio.go
+++ gpio.go
@@ -1,46 +1,45 @@
 package main
 
 import (
-	"github.com/stianeikeland/go-rpio"
-	"time"
-	"log"
+    "github.com/stianeikeland/go-rpio"
+    "time"
+    "log"
 )
 
 func init() {
-	rpio.Open()
+    rpio.Open()
 }
 
 func hwButton() {
-	high := 124.0
-	low  := 30.0
+    high := 124.0
+    low  := 30.0
 
-	button := rpio.Pin(26)
-	button.Input()
-	button.PullUp()
+    button := rpio.Pin(26)
+    button.Input()
+    button.PullUp()
 
-	for {
-		if button.Read() == 0 {
-			log.Println("button pressed")
-			if acc.Thermostat.TargetTemperature.GetValue() >= ( high - 10.0 ) {
-	    		setTargetTemp(low)
-			} else {
-	    		setTargetTemp(high)
-			}
-
-			time.Sleep(500 * time.Millisecond)
-		}
-		time.Sleep(50 * time.Millisecond)
-	}
+    for {
+        if button.Read() == 0 {
+            log.Println("button pressed")
+            if acc.Thermostat.TargetTemperature.GetValue() >= ( high - 10.0 ) {
+                setTargetTemp(low)
+            } else {
+                setTargetTemp(high)
+            }
+            time.Sleep(500 * time.Millisecond)
+         }
+         time.Sleep(50 * time.Millisecond)
+    }
 }
 
 func heaterControl(on bool) {
-	pin := rpio.Pin(14)
-	pin.Output()
-	
-	relayActivations.Inc()
-	if on{
-		pin.High()
-	} else {
-		pin.Low()
-	}
+    pin := rpio.Pin(14)
+    pin.Output()
+
+    relayActivations.Inc()
+    if on{
+        pin.High()
+    } else {
+        pin.Low()
+    }
 }
